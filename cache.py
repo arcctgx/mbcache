59ba96b9fab65bc225850a58875d6bb05aeba8a6
arcctgx <arcctgx@o2.pl>
2023-02-19 23:20:50 +0100

    correct issues reported by Pylint in cache.py
    Correct issues reported by Pylint (variable names, missing docstrings,
    and so on). Reformat code with YAPF.
    
    Remove hashbang, cache module is not supposed to be executable.

--- cache.py
+++ cache.py
@@ -1,12 +1,28 @@
-#!/usr/bin/env python3
+"""A simple cache for storing MusicBrainz recordings and releases."""
 
 import glob
 import json
 import os
 import time
+
 from xdg import BaseDirectory
 
+
 class RecordingCache:
+    """
+    Cache for storing recording MBIDs.
+
+    Cache is stored in JSON file in user's XDG cache directory. The path is
+    derived from application name and cache name, passed as arguments to the
+    class constructor.
+
+    Objects are stored in key-value format, where the key is derived from
+    artist name, track title and album title, and the value is the recording
+    MBID.
+
+    Cache stores times of last update and last lookup as UNIX timestamps.
+    """
+
     def __init__(self, application='mbcache', cache_name='recordings', sort_index=False):
         self.cache = {}
         self.update_required = False
@@ -15,26 +31,27 @@ class RecordingCache:
         self.cache_path = os.path.join(self.cache_dir, cache_name + '.json')
 
         try:
-            with open(self.cache_path, 'r') as f:
-                self.cache = json.load(f)
+            with open(self.cache_path, encoding='utf-8') as cache_file:
+                self.cache = json.load(cache_file)
             print('Loaded', len(self.cache), 'cache entries.')
         except FileNotFoundError:
             print('Cache file does not exist. Initializing empty cache.')
 
     def __del__(self):
         if self.update_required:
-            with open(self.cache_path, 'w') as f:
-                json.dump(self.cache, f, indent=1, sort_keys=self.sort_index)
+            with open(self.cache_path, 'w', encoding='utf-8') as cache_file:
+                json.dump(self.cache, cache_file, indent=1, sort_keys=self.sort_index)
 
     def __str__(self):
         return json.dumps(self.cache, indent=1)
 
     @staticmethod
-    def encode_key(artist, title, album):
+    def _encode_key(artist, title, album):
         return '\t'.join((artist, album, title)).casefold()
 
     def lookup(self, artist, title, album):
-        key = self.encode_key(artist, title, album)
+        """Look up a recording MBID by artist, title and album."""
+        key = self._encode_key(artist, title, album)
         try:
             entry = self.cache[key]
             entry['last_lookup'] = int(time.time())
@@ -44,17 +61,37 @@ class RecordingCache:
             return None
 
     def store(self, artist, title, album, mbid):
-        key = self.encode_key(artist, title, album)
-        value = {
-            'id': mbid,
-            'last_update': int(time.time()),
-            'last_lookup': None
-        }
+        """Store a recording MBID in cache."""
+        key = self._encode_key(artist, title, album)
+        value = {'id': mbid, 'last_update': int(time.time()), 'last_lookup': None}
         self.cache[key] = value
         self.update_required = True
 
 
 class ReleaseCache:
+    """
+    Cache for storing MusicBrainz release data.
+
+    Cache is stored in user's XDG cache directory, in subdirectory whose path
+    is derived from application name and cache name passed as arguments to the
+    class constructor. It contains JSON files with release data, whose names
+    are derived from MusicBrainz release MBIDs. It additionally contains a
+    key-value index file, which maps keys to release MBIDs.
+
+    Index keys are derived from artist names and release titles, which must be
+    unique. To make it possible to keep multiple versions of the same album in
+    cache, an optional disambiguation string can be provided to distinguish
+    otherwise indentically named releases. If disambiguation string is not
+    provided when another release with the same name artist and title is stored
+    in cache, the cache entry will be replaced and old release JSON file will
+    be removed.
+
+    Cache stores times of last update and last lookup as UNIX timestamps. Also
+    stored is the permanence information. If cache entry is set as permanent,
+    it will never be invalidated or updated automatically (but can still be
+    replaced as described above).
+    """
+
     def __init__(self, application='mbcache', cache_name='releases', sort_index=False):
         self.cache = None
         self.update_required = False
@@ -63,27 +100,31 @@ class ReleaseCache:
         self.index_path = os.path.join(self.cache_dir, 'index.json')
 
         try:
-            with open(self.index_path) as f:
-                self.cache = json.load(f)
+            with open(self.index_path, encoding='utf-8') as cache_index:
+                self.cache = json.load(cache_index)
             print('Loaded', len(self.cache), 'cache entries.')
         except FileNotFoundError:
-            self.cache = dict()
+            self.cache = {}
             print('Cache index does not exist. Initialized empty cache.')
 
     def __del__(self):
         if self.cache is not None:
             if self.update_required:
-                with open(self.index_path, 'w') as f:
-                    json.dump(self.cache, f, indent=1, sort_keys=self.sort_index)
+                with open(self.index_path, 'w', encoding='utf-8') as cache_index:
+                    json.dump(self.cache, cache_index, indent=1, sort_keys=self.sort_index)
 
-            self.remove_orphans()
+            self._remove_orphans()
 
     def __str__(self):
         return json.dumps(self.cache, indent=1)
 
-    def remove_orphans(self):
-        in_cache = set(glob.glob(os.path.join(self.cache_dir, '????????-????-????-????-????????????.json')))
-        in_index = set([os.path.join(self.cache_dir, entry['id'] + '.json') for entry in self.cache.values()])
+    def _remove_orphans(self):
+        in_cache = set(
+            glob.glob(os.path.join(self.cache_dir, '????????-????-????-????-????????????.json')))
+        in_index = {
+            os.path.join(self.cache_dir, entry['id'] + '.json')
+            for entry in self.cache.values()
+        }
         orphans = in_cache - in_index
         num_orphans = len(orphans)
 
@@ -94,14 +135,18 @@ class ReleaseCache:
             print('Removed', num_orphans, 'orphaned cache files.')
 
     @staticmethod
-    def encode_key(artist, title, disambiguation=None):
+    def _encode_key(artist, title, disambiguation=None):
         if disambiguation is None:
             return '\t'.join((artist, title)).casefold()
 
         return '\t'.join((artist, title, disambiguation)).casefold()
 
     def lookup(self, artist, title, disambiguation=None):
-        key = self.encode_key(artist, title, disambiguation)
+        """
+        Look up release data by artist, title
+        and optional disambiguation string.
+        """
+        key = self._encode_key(artist, title, disambiguation)
         try:
             entry = self.cache[key]
             entry['last_lookup'] = int(time.time())
@@ -113,12 +158,13 @@ class ReleaseCache:
         release_file = os.path.join(self.cache_dir, mbid + '.json')
 
         try:
-            with open(release_file) as f:
-                return json.load(f)
+            with open(release_file, encoding='utf-8') as rel:
+                return json.load(rel)
         except FileNotFoundError:
             return None
 
     def lookup_id(self, mbid):
+        """Look up release information by MBID."""
         index_mbids = [entry['id'] for entry in self.cache.values()]
         if mbid not in index_mbids:
             return None
@@ -131,14 +177,15 @@ class ReleaseCache:
         release_file = os.path.join(self.cache_dir, mbid + '.json')
 
         try:
-            with open(release_file) as f:
-                return json.load(f)
+            with open(release_file, encoding='utf-8') as rel:
+                return json.load(rel)
         except FileNotFoundError:
             return None
 
     def store(self, artist, title, release_data, disambiguation=None):
+        """Store release data in cache, with optional disambiguation string."""
         mbid = release_data['id']
-        key = self.encode_key(artist, title, disambiguation)
+        key = self._encode_key(artist, title, disambiguation)
         value = {
             'id': mbid,
             'last_update': int(time.time()),
@@ -150,5 +197,5 @@ class ReleaseCache:
 
         release_file = os.path.join(self.cache_dir, mbid + '.json')
 
-        with open(release_file, 'w') as f:
-            json.dump(release_data, f, indent=0)
+        with open(release_file, 'w', encoding='utf-8') as rel:
+            json.dump(release_data, rel, indent=0)
